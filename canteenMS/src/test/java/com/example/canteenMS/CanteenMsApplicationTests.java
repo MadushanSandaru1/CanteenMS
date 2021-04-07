dc61b5c526d63ee31a8f2ba29b549b20f08f2628
Mithila <mithilamadusanka97@gmail.com>
2021-04-07 16:42:53 +0530

    changed

---
+++ canteenMS/src/test/java/com/example/canteenMS/CanteenMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canteenMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
