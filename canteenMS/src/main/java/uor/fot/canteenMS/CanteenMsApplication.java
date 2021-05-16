6072330241779720d86421389b39fd468df0980f
Madushan Sandaruwan <madushansandaru1@gmail.com>
2021-05-16 23:24:31 +0530

    upload

---
+++ canteenMS/src/main/java/uor/fot/canteenMS/CanteenMsApplication.java
@@ -0,0 +1,13 @@
+package uor.fot.canteenMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenMsApplication.class, args);
+	}
+
+}
---
+++ canteenMS/src/test/java/uor/fot/canteenMS/CanteenMsApplicationTests.java
@@ -0,0 +1,13 @@
+package uor.fot.canteenMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
